1f6abceeafbff3132a23e49995bcc1d768faa9c0
victor01ondoua <victor01ondoua@gmail.com>
2020-09-24 10:24:05 +0100

    intent Map & intent phone

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -28,16 +29,29 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
 
-    
+    public void createMapIntent(View view){
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if(mapIntent.resolveActivity(getPackageManager())!=null){
+            startActivity(mapIntent);
+        }
+
+    }
+
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
 
-    public void createPhoneIntent(){
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
 
-        Intent phoneIntent = new Intent(Intent.EXTRA_PHONE_NUMBER);
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uri);
         startActivity(phoneIntent);
+
     }
 
 }
